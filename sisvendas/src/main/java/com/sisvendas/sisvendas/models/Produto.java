cd5bca19fe66359edf17cf699e41ecb620752a77
Petherson Erasmo <petherson_erasmo@hotmail.com>
2024-03-04 21:18:12 -0300

    Adiciona arquivos iniciais do projeto sisvendas

---
+++ sisvendas/src/main/java/com/sisvendas/sisvendas/models/Produto.java
@@ -0,0 +1,32 @@
+package com.sisvendas.sisvendas.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PRODUTO")
+public class Produto extends BaseModel {
+    private String descricao;
+    private String unidade;
+    private String preco;
+    
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getUnidade() {
+        return unidade;
+    }
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+    public String getPreco() {
+        return preco;
+    }
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+}
